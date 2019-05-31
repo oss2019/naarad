329b19dee564aa3ed6a1d2d2d60e19637a665e5d
HARSHIT JAIN <44135316+iharshit009@users.noreply.github.com>
2019-05-31 10:32:44 +0530

    updated file

--- open_group.py
+++ open_group.py
@@ -10,18 +10,17 @@ class Facebook:
         self.username = input("enter your username: ")
         self.password = getpass.getpass("enter your password: ")
         self.link = input("enter the link:")
+        self.driver = webdriver.Chrome()
+        self.fb_login()
 
-    def group_page(self):
-        self.driver.get(self.link)
+    def open_group(self, input_link):
+        self.driver.get(input_link)
 
     def fb_login(self):
-
-        self.driver = webdriver.Chrome()
         self.driver.get("https://www.facebook.com/")
         self.driver.find_element_by_id('email').send_keys(self.username)
         self.driver.find_element_by_id('pass').send_keys(self.password)
         self.driver.find_element_by_id('loginbutton').click()
-        self.group_page()
+        self.open_group(self.link)
 
-
-Facebook().fb_login()
+Facebook()
